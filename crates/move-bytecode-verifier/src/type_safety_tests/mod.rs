cc15bb38d0bf6fa5b9d6d9c31bdc088a79181599
Valerii Huhnin <olympichek1@gmail.com>
2024-08-16 05:53:14 +0000

    add tests for `BrFalse` instruction

--- crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
+++ crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
@@ -46,8 +46,9 @@ fn get_fun_context(module: &CompiledModule) -> FunctionContext {
     )
 }
 
+
 #[test]
-fn test_br_true_bool() {
+fn test_br_true_correct_type() {
     let code = vec![Bytecode::LdTrue, Bytecode::BrTrue(0)];
     let module = make_module(code);
     let fun_context = get_fun_context(&module);
@@ -56,7 +57,7 @@ fn test_br_true_bool() {
 }
 
 #[test]
-fn test_br_true_u32() {
+fn test_br_true_wrong_type() {
     let code = vec![Bytecode::LdU32(0), Bytecode::BrTrue(0)];
     let module = make_module(code);
     let fun_context = get_fun_context(&module);
@@ -75,3 +76,35 @@ fn test_br_true_no_arg() {
     let fun_context = get_fun_context(&module);
     let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
 }
+
+
+#[test]
+fn test_br_false_correct_type() {
+    let code = vec![Bytecode::LdTrue, Bytecode::BrFalse(0)];
+    let module = make_module(code);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_br_false_wrong_type() {
+    let code = vec![Bytecode::LdU32(0), Bytecode::BrFalse(0)];
+    let module = make_module(code);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::BR_TYPE_MISMATCH_ERROR
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_br_false_no_arg() {
+    let code = vec![Bytecode::BrFalse(0)];
+    let module = make_module(code);
+    let fun_context = get_fun_context(&module);
+    let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+}
+
