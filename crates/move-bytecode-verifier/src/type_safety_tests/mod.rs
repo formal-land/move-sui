f91cdf2f630e3711774fe16cd1e8a5fca7753e3a
Valerii Huhnin <olympichek1@gmail.com>
2024-09-04 08:10:05 +0000

    tests for `MoveToDeprecated` instructions

--- crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
+++ crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
@@ -2246,6 +2246,112 @@ fn test_move_from_deprecated_no_arg() {
 }
 
 #[test]
+fn test_move_to_deprecated_correct_type() {
+    let code = vec![
+        Bytecode::ImmBorrowLoc(0),
+        Bytecode::LdU32(42),
+        Bytecode::Pack(StructDefinitionIndex(0)),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_move_to_deprecated_mismatched_types() {
+    let code = vec![
+        Bytecode::ImmBorrowLoc(0),
+        Bytecode::LdU32(42),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVETO_TYPE_MISMATCH_ERROR
+    );
+}
+
+#[test]
+fn test_move_to_deprecated_wrong_type() {
+    let code = vec![
+        Bytecode::MutBorrowLoc(0),
+        Bytecode::LdU32(42),
+        Bytecode::Pack(StructDefinitionIndex(0)),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVETO_TYPE_MISMATCH_ERROR
+    );
+
+    let code = vec![
+        Bytecode::ImmBorrowLoc(0),
+        Bytecode::LdU32(42),
+        Bytecode::Pack(StructDefinitionIndex(0)),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::U32);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVETO_TYPE_MISMATCH_ERROR
+    );
+}
+
+#[test]
+fn test_move_to_deprecated_no_key() {
+    let code = vec![
+        Bytecode::ImmBorrowLoc(0),
+        Bytecode::LdU32(42),
+        Bytecode::Pack(StructDefinitionIndex(0)),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::PRIMITIVES);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVETO_WITHOUT_KEY_ABILITY
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_move_to_too_few_args() {
+    let code = vec![
+        Bytecode::ImmBorrowLoc(0),
+        Bytecode::MoveToDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+}
+
+#[test]
+#[should_panic]
+fn test_move_to_deprecated_no_arg() {
+    let code = vec![Bytecode::MoveToDeprecated(StructDefinitionIndex(0))];
+    let mut module = make_module_with_local(code, SignatureToken::Signer);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+}
+
+#[test]
 fn test_borrow_global_deprecated_correct_type() {
     for instr in vec![
         Bytecode::ImmBorrowGlobalDeprecated(StructDefinitionIndex(0)),
