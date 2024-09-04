69ef9c90d566ed1a5abdfe8b091f7850f62efcaa
Valerii Huhnin <olympichek1@gmail.com>
2024-09-04 06:14:50 +0000

    tests for `MoveFromDeprecated` instruction

--- crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
+++ crates/move-bytecode-verifier/src/type_safety_tests/mod.rs
@@ -2189,3 +2189,58 @@ fn test_exists_deprecated_no_arg() {
     let fun_context = get_fun_context(&module);
     let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
 }
+
+#[test]
+fn test_move_from_deprecated_correct_type() {
+    let code = vec![
+        Bytecode::CopyLoc(0),
+        Bytecode::MoveFromDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Address);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_move_from_deprecated_wrong_type() {
+    let code = vec![
+        Bytecode::LdU64(42),
+        Bytecode::MoveFromDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Address);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVEFROM_TYPE_MISMATCH_ERROR
+    );
+}
+
+#[test]
+fn test_move_from_deprecated_no_key() {
+    let code = vec![
+        Bytecode::CopyLoc(0),
+        Bytecode::MoveFromDeprecated(StructDefinitionIndex(0)),
+    ];
+    let mut module = make_module_with_local(code, SignatureToken::Address);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::PRIMITIVES);
+    let fun_context = get_fun_context(&module);
+    let result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+    assert_eq!(
+        result.unwrap_err().major_status(),
+        StatusCode::MOVEFROM_WITHOUT_KEY_ABILITY
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_move_from_deprecated_no_arg() {
+    let code = vec![Bytecode::MoveFromDeprecated(StructDefinitionIndex(0))];
+    let mut module = make_module_with_local(code, SignatureToken::Address);
+    add_simple_struct_with_abilities(&mut module, AbilitySet::ALL);
+    let fun_context = get_fun_context(&module);
+    let _result = type_safety::verify(&module, &fun_context, &mut DummyMeter);
+}
